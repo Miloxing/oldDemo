b0659037c9a53dd727a05eacfbf973253dcfa768
yukminnie <dadiaomin@126.com>
2022-12-05 16:38:48 +0800

    delete

--- projects/craw_3/xiaozhanSpider/pop.py
+++
@@ -1,9 +0,0 @@
-list = []
-with open(r'C:\Users\M\Desktop\alixiaozhan\other.txt', 'r') as f:
-    list.extend(f.readlines())
-
-for l in list:
-    if l.endswith('.html\n'):
-        pass
-    else:
-        print(l)
